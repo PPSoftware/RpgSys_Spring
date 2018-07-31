6f83f88d3dfe2c3602d6408bc6cbe73567f433bb
Rafael <rafael210587@gmail.com>
2018-07-30 22:54:16 -0300

    Delete and remade UserDao

--- src/main/java/io/github/ppsoftware/rpgSys/models/IUserDao.java
+++ src/main/java/io/github/ppsoftware/rpgSys/models/IUserDao.java
@@ -3,18 +3,10 @@ package io.github.ppsoftware.rpgSys.models;
 import java.io.Serializable;
 import java.util.List;
 
-public interface IUserDao <T extends Serializable> {
+import org.springframework.data.repository.CrudRepository;
+
+public interface IUserDao<T> extends CrudRepository<UserDAO, T >{
 	
-	T findOne(final long id);
-	 
-	   List<T> findAll();
-	 
-	   void create(final T entity);
-	 
-	   T update(final T entity);
-	 
-	   void delete(final T entity);
-	 
-	   void deleteById(final long entityId);
+
 
 }
--- src/main/java/io/github/ppsoftware/rpgSys/models/UserDAO.java
+++ src/main/java/io/github/ppsoftware/rpgSys/models/UserDAO.java
@@ -1,13 +1,86 @@
 package io.github.ppsoftware.rpgSys.models;
 
 import java.io.Serializable;
+import java.util.Date;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Repository;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
-@Repository
-@Scope( BeanDefinition.SCOPE_PROTOTYPE )
-public class UserDAO< T extends Serializable >
-  extends AbstractHibernateDao< T > implements IUserDao< T >{
+@Entity
+public class UserDAO implements Serializable {
    //
+
+	    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -2257996512395408133L;
+
+
+		@Id
+	    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_Sequence")
+	    @SequenceGenerator(name = "user_Sequence", sequenceName = "USER_SEQ")
+	    @Column(name = "USER_ID")
+	    private long userId;
+	    
+	    
+	    @Column(name = "LOGIN")
+	    private String login;
+	    
+	    @Column(name = "PASSWORD")
+	    private String password;
+	    
+	    @Column(name = "CREATED_AT")
+	    private Date created_at;
+	    @Column(name = "EMAIL")
+	    private String email;
+	    public long getUserId() {
+			return userId;
+		}
+
+		public void setUserId(long userId) {
+			this.userId = userId;
+		}
+
+		public String getLogin() {
+			return login;
+		}
+
+		public void setLogin(String login) {
+			this.login = login;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+
+		public Date getCreated_at() {
+			return created_at;
+		}
+
+		public void setCreated_at(Date created_at) {
+			this.created_at = created_at;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		
+	    
+	    public UserDAO() {
+	    }
+
+
 }
